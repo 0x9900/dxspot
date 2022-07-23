6179a0f60fef037a26c958ab58201d69829c1b23
Fred C <github-fred@hidzz.com>
2022-07-23 12:03:51 -0700

    Fix indentation; use the default style

--- dxspots.py
+++ dxspots.py
@@ -4,7 +4,6 @@ import argparse
 import logging
 import os
 import sqlite3
-import sys
 
 from collections import defaultdict
 from datetime import datetime
@@ -12,89 +11,86 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
-from matplotlib.dates import DateFormatter, HourLocator, DayLocator, MinuteLocator
-from matplotlib.ticker import AutoMinorLocator, MultipleLocator
+from matplotlib.dates import DateFormatter, HourLocator
+from matplotlib.ticker import AutoMinorLocator
 
-from scipy.interpolate import make_interp_spline, BSpline
-
-STYLE = 'ggplot'
-plt.style.use(STYLE)
+from scipy.interpolate import make_interp_spline
 
 BUCKET_SIZE = 2
 
 bucket = lambda x: int(BUCKET_SIZE * int(x.hour / BUCKET_SIZE))
 
 def read_data(dbname):
-    logger.info('Reading data from: %s', dbname)
-    conn = sqlite3.connect(dbname, timeout=3)
-    data = {}
-    result = conn.execute('select * from dxspot')
-    for row in result:
-        if row[4] == '':
-            continue
-        _date = datetime.fromtimestamp(row[7])
-        date = _date.replace(hour=bucket(_date), minute=0, second=0, microsecond=0)
-        if date not in data:
-            data[date] = defaultdict(int)
-        data[date][row[4]] += 1
+  logger.info('Reading data from: %s', dbname)
+  conn = sqlite3.connect(dbname, timeout=3)
+  data = {}
+  result = conn.execute('select * from dxspot')
+  for row in result:
+    if row[4] == '':
+      continue
+    _date = datetime.fromtimestamp(row[7])
+    date = _date.replace(hour=bucket(_date), minute=0, second=0, microsecond=0)
+    if date not in data:
+      data[date] = defaultdict(int)
+    data[date][row[4]] += 1
 
-    return sorted(data.items())
+  return sorted(data.items())
 
 
 def graph(data, target_dir):
-    graphname = os.path.join(target_dir, 'dxcc-stats.svg')
-    keys = ['EU', 'AS', 'OC', 'NA', 'SA', 'AF']
-    continents = {}
+  graphname = os.path.join(target_dir, 'dxcc-stats.svg')
+  keys = ['EU', 'AS', 'OC', 'NA', 'SA', 'AF']
+  continents = {}
 
-    logger.info('Generating graph file: %s', graphname)
+  logger.info('Generating graph file: %s', graphname)
 
-    labels = np.array([d[0].timestamp() for d in data])
-    x = np.linspace(labels.min(), labels.max(), 1500)
+  labels = np.array([d[0].timestamp() for d in data])
+  xdata = np.linspace(labels.min(), labels.max(), 1500)
 
-    for ctn in keys:
-        y = np.array([d[1][ctn] for d in data])
-        spl = make_interp_spline(labels, y, k=5)
-        y = spl(x)
-        y[y < 0] = 0
-        continents[ctn] = y
+  for ctn in keys:
+    ydata = np.array([d[1][ctn] for d in data])
+    spl = make_interp_spline(labels, ydata, k=5)
+    ydata = spl(xdata)
+    ydata[ydata < 0] = 0
+    continents[ctn] = ydata
 
-    fig, ax = plt.subplots(figsize=(12, 5))
+  _, axgc = plt.subplots(figsize=(12, 5))
 
-    x = [datetime.fromtimestamp(d) for d in x]
-    labels = [datetime.fromtimestamp(d) for d in labels]
+  xdata = np.array([datetime.fromtimestamp(d) for d in xdata])
+  labels = np.array([datetime.fromtimestamp(d) for d in labels])
 
-    formatter = DateFormatter('%Y-%m-%d')
-    plt.title('DX Spots / Continent', fontsize=18)
+  formatter = DateFormatter('%Y-%m-%d')
+  plt.title('DX Spots / Continent', fontsize=18)
 
-    for key in keys:
-        plt.plot(x, continents[key], linewidth=1.5, label=key)
+  for key in keys:
+    plt.plot(xdata, continents[key], linewidth=1.5, label=key)
 
-    total = np.sum(np.array([x for x in continents.values()]), axis=0)
-    plt.plot(x, total, linewidth=.5, label='Total', color='darkgray')
+  total = np.sum(np.array(list(continents.values())), axis=0)
+  plt.plot(xdata, total, linewidth=.5, label='Total', color='gray')
 
-    ax.xaxis.set_major_formatter(formatter)
-    ax.xaxis.set_tick_params(rotation=10, labelsize=10)
-    ax.xaxis.set_minor_locator(HourLocator())
-    ax.xaxis.set_minor_locator(AutoMinorLocator(6))
-    ax.set_xlabel('Dates (UTC)')
-    ax.set_ylabel('Numer of spots')
+  axgc.xaxis.set_major_formatter(formatter)
+  axgc.xaxis.set_tick_params(rotation=10, labelsize=10)
+  axgc.xaxis.set_minor_locator(HourLocator())
+  axgc.xaxis.set_minor_locator(AutoMinorLocator(6))
+  axgc.set_xlabel('Dates (UTC)')
+  axgc.set_ylabel('Numer of spots')
 
-    ax.grid(True)
+  axgc.grid(True)
 
-    plt.legend(fontsize=10, facecolor='white', framealpha=.5)
-    plt.savefig(graphname, transparent=False, dpi=72)
+  plt.legend(fontsize=10, facecolor='white')
+  plt.savefig(graphname, transparent=False, dpi=72)
 
 
-def main(args=sys.argv[:1]):
-    parser = argparse.ArgumentParser(description='Graph dxcc trafic')
-    parser.add_argument("--target-dir", default='/tmp',
-                        help="Where to copy the graph")
-    parser.add_argument("--database", required=True, help="Sqlite3 database path")
-    opts = parser.parse_args()
-    data = read_data(opts.database)
-    graph(data, opts.target_dir)
+def main():
+  parser = argparse.ArgumentParser(description='Graph dxcc trafic')
+  parser.add_argument("--target-dir", default='/tmp',
+                      help="Where to copy the graph")
+  parser.add_argument("--database", required=True, help="Sqlite3 database path")
+  opts = parser.parse_args()
+  data = read_data(opts.database)
+  graph(data, opts.target_dir)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
-    main()
+  logging.basicConfig(level=logging.INFO)
+  logger = logging.getLogger(__name__)
+  main()
